ece26c1497b153d4be881eaa28ab89291fab7896
chanz40000 <21130574@st.hcmuaf.edu.vn>
2025-03-10 23:41:45 +0700

    first commit

---
+++ src/test/java/com/example/cdwebbackend/CdWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
