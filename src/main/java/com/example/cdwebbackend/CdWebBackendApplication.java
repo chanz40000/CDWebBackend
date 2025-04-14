bf8a547ff414bafad0343450823e9eb5dc9ab3b1
chanz40000 <21130574@st.hcmuaf.edu.vn>
2025-04-14 23:42:34 +0700

    api gui mail

--- src/main/java/com/example/cdwebbackend/CdWebBackendApplication.java
+++ src/main/java/com/example/cdwebbackend/CdWebBackendApplication.java
@@ -1,14 +1,27 @@
 package com.example.cdwebbackend;
+import com.example.cdwebbackend.service.impl.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.example.cdwebbackend")
 @EnableJpaRepositories(basePackages = "com.example.cdwebbackend.repository")
 
 public class CdWebBackendApplication {
+    @Autowired
+    private EmailSenderService senderService;
+
     public static void main(String[] args) {
         SpringApplication.run(CdWebBackendApplication.class, args);
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail(){
+        senderService.sendEmail("ntchanz4000@gmail.com",
+                "This is test email",
+                "this is body of email");
     }
 }
